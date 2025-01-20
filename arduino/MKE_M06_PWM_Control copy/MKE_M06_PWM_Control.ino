1765016086f43257cd4eb46e6df4431589d556ef
huy ngo <huydr68@gmail.com>
2025-01-20 11:05:38 +0700

    update

---
+++ arduino/MKE_M06_PWM_Control copy/MKE_M06_PWM_Control.ino
@@ -0,0 +1,17 @@
+
+// Đặt tên cho chân kết nối Module
+const int SIG_PIN 10; // khai báo chân số 10 là chân điều khiển module MKE-M06
+
+void setup()
+{
+
+  pinMode(SIG_PIN, OUTPUT); // cấu hình chân SIG la chân tín hiệu điều khiển module MKE-M06
+}
+
+void loop()
+{
+  int Analog_Data = analogRead(A1);           // Đọc dữ liệu  từ chân A1
+  analogWrite(SIG_PIN, Analog_Data);          // ghi dữ liệu đọc được từ chân A1 vào chân tín hiệu điều khiển module MKE-M06
+  int State_BUTTON = digitalRead(BUTTON_PIN); // Đọc trạng thái nút nhấn
+  delay(100);
+}
