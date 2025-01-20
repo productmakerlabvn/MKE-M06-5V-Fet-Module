d50072acbcfae461fedd7492a26c083e0eedc798
huy ngo <huydr68@gmail.com>
2025-01-20 14:20:07 +0700

    update

--- arduino/MKE_M06_Button_Control/MKE_M06_Button_Control.ino
+++ arduino/MKE_M06_Button_Control/MKE_M06_Button_Control.ino
@@ -1,14 +1,14 @@
 
 // Đặt tên cho chân kết nối Module
-const int BUTTON_PIN 9; //khai báo chân số 9 là chân nút nhấn
-const int SIG_PIN 10;  // khai báo chân số 10 là chân điều khiển module MKE-M06
+const int BUTTON_PIN = 11; // khai báo chân số 9 là chân nút nhấn
+const int SIG_PIN = 10;    // khai báo chân số 10 là chân điều khiển module MKE-M06
 
 void setup()
 {
 
-  pinMode(BUTTON_PIN, INPUT);//cấu hình chân button là chân nhân tín hiệu từ nút nhấn
+  pinMode(BUTTON_PIN, INPUT); // cấu hình chân button là chân nhân tín hiệu từ nút nhấn
 
-  pinMode(SIG_PIN, OUTPUT); //cấu hình chân SIG la chân tín hiệu điều khiển module MKE-M06
+  pinMode(SIG_PIN, OUTPUT); // cấu hình chân SIG la chân tín hiệu điều khiển module MKE-M06
 }
 
 void loop()
@@ -22,6 +22,6 @@ void loop()
   }
   else if (State_BUTTON == LOW) // Nút nhấn được nhấn
   {
-    digitalWrite(SIG_PIN, HIGH);// Bật máy bơm
+    digitalWrite(SIG_PIN, HIGH); // Bật máy bơm
   }
 }
