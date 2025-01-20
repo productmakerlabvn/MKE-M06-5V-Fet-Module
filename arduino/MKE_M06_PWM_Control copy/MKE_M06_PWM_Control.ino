2799541fff044719c7b150bd1ef000fe3c3ea9d4
huy ngo <huydr68@gmail.com>
2025-01-20 12:22:31 +0700

    update

--- arduino/MKE_M06_PWM_Control copy/MKE_M06_PWM_Control.ino
+++ arduino/MKE_M06_PWM_Control copy/MKE_M06_PWM_Control.ino
@@ -11,6 +11,7 @@ void setup()
 void loop()
 {
   int Analog_Data = analogRead(A1);           // Đọc dữ liệu  từ chân A1
+ int PWM_Data = map(Analog_Data, 0, 1023, 0, 255);
   analogWrite(SIG_PIN, Analog_Data);          // ghi dữ liệu đọc được từ chân A1 vào chân tín hiệu điều khiển module MKE-M06
   int State_BUTTON = digitalRead(BUTTON_PIN); // Đọc trạng thái nút nhấn
   delay(100);
